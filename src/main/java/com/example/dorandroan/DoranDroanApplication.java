6549c47401bfa9d4facc3cc3e0430e4c68d1095b
nayoung <yny3533@naver.com>
2025-02-17 17:36:15 +0900

    Initial Commit

---
+++ src/main/java/com/example/dorandroan/DoranDroanApplication.java
@@ -0,0 +1,13 @@
+package com.example.dorandroan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoranDroanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoranDroanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dorandroan/DoranDroanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dorandroan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoranDroanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
