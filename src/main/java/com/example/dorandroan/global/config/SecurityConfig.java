6daef64159f7f4125a0e863e18bb5ea38a04e303
nayoung <yny3533@naver.com>
2025-03-10 21:10:12 +0900

    [feat] CORS 설정 추가

--- src/main/java/com/example/dorandroan/global/config/SecurityConfig.java
+++ src/main/java/com/example/dorandroan/global/config/SecurityConfig.java
@@ -15,9 +15,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
@@ -63,18 +60,4 @@ public class SecurityConfig {
 
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(corsOrigin);
-        configuration.setAllowedMethods(
-                List.of("GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setMaxAge(3600L);
-        configuration.setAllowCredentials(true);
-        configuration.setExposedHeaders(List.of("Access-Token"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
