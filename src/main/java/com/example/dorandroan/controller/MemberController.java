e2093de00af0d2d5bfc8f2e7c2f42dc22c83863b
nayoung <yny3533@naver.com>
2025-03-21 01:26:47 +0900

    [chore] 디버깅중..

--- src/main/java/com/example/dorandroan/controller/MemberController.java
+++ src/main/java/com/example/dorandroan/controller/MemberController.java
@@ -58,7 +58,54 @@ public class MemberController {
 
     @PostMapping("/mypage")
     public ResponseEntity<Void> myPage(HttpServletRequest request) {
+// 1. 요청 메서드 (GET, POST, 등)
+        System.out.println("Request Method: " + request.getMethod());
 
+        // 2. 요청 URI
+        System.out.println("Request URI: " + request.getRequestURI());
+
+        // 3. 요청 URL
+        System.out.println("Request URL: " + request.getRequestURL());
+
+        // 4. 쿼리 파라미터 출력
+        System.out.println("Query Parameters: ");
+        Enumeration<String> paramNames = request.getParameterNames();
+        while (paramNames.hasMoreElements()) {
+            String paramName = paramNames.nextElement();
+            String[] paramValues = request.getParameterValues(paramName);
+            for (String value : paramValues) {
+                System.out.println(paramName + " = " + value);
+            }
+        }
+
+        // 5. 요청 헤더 출력
+        System.out.println("Request Headers: ");
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            String headerValue = request.getHeader(headerName);
+            System.out.println(headerName + " = " + headerValue);
+        }
+
+        // 6. 요청 속성 출력
+        System.out.println("Request Attributes: ");
+        Enumeration<String> attributeNames = request.getAttributeNames();
+        while (attributeNames.hasMoreElements()) {
+            String attributeName = attributeNames.nextElement();
+            Object attributeValue = request.getAttribute(attributeName);
+            System.out.println(attributeName + " = " + attributeValue);
+        }
+
+        // 7. 쿠키 출력
+        System.out.println("Cookies: ");
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println("Cookie: " + cookie.getName() + " = " + cookie.getValue());
+            }
+        }
+
+        // 8. 요청의 사용자 정보 (Principal, 인증된 사용자)
+        System.out.println("User Principal: " + request.getUserPrincipal());
         return ResponseEntity.ok().build();
     }
 }
