1020aa0de208097ff4a5f594794d87c0558e665a
nayoung <yny3533@naver.com>
2025-03-24 19:19:16 +0900

    [fix] cors 에러 해결

--- src/main/java/com/example/dorandroan/global/jwt/JwtAuthFilter.java
+++ src/main/java/com/example/dorandroan/global/jwt/JwtAuthFilter.java
@@ -34,7 +34,6 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = cookieUtil.getAccessFromCookie(request);
-        System.out.println("Now access" + token);
 
         if (token != null) {
             try {
