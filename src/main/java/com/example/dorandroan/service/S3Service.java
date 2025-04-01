13c43187e1df57ccae7cf9e482078bc455027f0f
nayoung <yny3533@naver.com>
2025-04-01 16:10:03 +0900

    [feat] S3 설정

---
+++ src/main/java/com/example/dorandroan/service/S3Service.java
@@ -0,0 +1,44 @@
+package com.example.dorandroan.service;
+
+import com.example.dorandroan.dto.ImageRequestDto;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+    private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    public String generatePresignedUrl(ImageRequestDto requestDto) {
+        String objectKey = generateFileName(requestDto.getFileName());
+
+        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(
+                PutObjectPresignRequest.builder()
+                        .signatureDuration(Duration.ofMinutes(10))  // URL 유효 시간
+                        .putObjectRequest(PutObjectRequest.builder().bucket(bucketName).key(objectKey).build())
+                        .build()
+        );
+
+        return presignedRequest.url().toString();
+
+    }
+
+    private String generateFileName(String originalFileName) {
+        return UUID.randomUUID() + "-" + originalFileName;
+    }
+
+}
