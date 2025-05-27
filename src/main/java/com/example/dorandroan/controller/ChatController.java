b4b38f79ced9513d6d50a4c5545904a561b16e01
nayoung <yny3533@naver.com>
2025-05-27 20:08:57 +0900

    [chore] 채팅 수신안됨 해결 위한 log 추가

--- src/main/java/com/example/dorandroan/controller/ChatController.java
+++ src/main/java/com/example/dorandroan/controller/ChatController.java
@@ -3,6 +3,7 @@ package com.example.dorandroan.controller;
 import com.example.dorandroan.dto.ChatDto;
 import com.example.dorandroan.service.ChatService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -10,6 +11,7 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class ChatController {
@@ -17,7 +19,9 @@ public class ChatController {
     @MessageMapping("/group/{roomId}")
     public void sendGroupMessage(@DestinationVariable Long roomId, ChatDto chatDto, Message<?> messageObj) {
         SimpMessageHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(messageObj, SimpMessageHeaderAccessor.class);
-        chatService.sendGroupMessage(roomId, (Long) accessor.getSessionAttributes().get("memberId"), chatDto);
+        Long memberId = (Long) accessor.getSessionAttributes().get("memberId");
+        log.info("Group Chatting received at Server {}, {}, {}", roomId, memberId, chatDto.getContent());
+        chatService.sendGroupMessage(roomId, memberId, chatDto);
     }
 
     @MessageMapping("/private/{roomId}")
