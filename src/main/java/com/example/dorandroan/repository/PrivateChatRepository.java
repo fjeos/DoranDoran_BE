4e82ad2a15b8c9525f972b02bb2b267632f283a2
nayoung <yny3533@naver.com>
2025-06-23 15:25:09 +0900

    [feat] 안 읽은 개수 처리 추가, max -> now part in 수정

--- src/main/java/com/example/dorandroan/repository/PrivateChatRepository.java
+++ src/main/java/com/example/dorandroan/repository/PrivateChatRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,4 +21,5 @@ public interface PrivateChatRepository extends MongoRepository<PrivateChat, Stri
 
     List<Chat> findByChatRoomIdOrderById(Long chatRoomId, Pageable pageable);
 
+    int countBySendAtBetween(Instant leaveTime, Instant enterTime);
 }
